3d8edb7cbf1ea0bbcd3a2f61070f46879805ba7d
csy100 <csiyu100@gmail.com>
2025-10-18 22:08:38 +0800

    feat:init project

---
+++ alpha-builder-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
